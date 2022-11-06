3464e816d219cc654a85b9f25a4c300ed7708653
Matheus L <mathfrlima@gmail.com>
2022-11-06 19:17:04 -0300

    completando o calendário

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,10 +23,19 @@ function createCard(date, day, games) {
 
 document.querySelector('#cards').innerHTML = 
   createCard('24/11', 'quinta', 
+  createGame('switzerland', '07:00', 'cameroon') + 
+  createGame('uruguai', '10:00', 'southkorea') + 
+  createGame('portugal', '13:00', 'ghana') +
   createGame('brazil', '16:00', 'serbia')) +
 
   createCard('28/11', 'segunda', 
+  createGame('cameroon', '07:00', 'serbia') +
+  createGame('southkorea', '10:00', 'ghana') +
   createGame('brazil', '13:00', 'switzerland') + 
   createGame('portugal', '16:00', 'uruguai')) +
 
-  createCard('02/12', 'sexta', createGame('brazil', '16:00', 'cameroon'))
+  createCard('02/12', 'sexta', 
+  createGame('southkorea', '07:00', 'portugal') +
+  createGame('ghana', '10:00', 'uruguai') +
+  createGame('serbia', '13:00', 'switzerland') +
+  createGame('brazil', '16:00', 'cameroon'))
